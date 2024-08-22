acef786b62767db1795f43998cfd55edad212982
su1415 <suyams@gmail.com>
2024-08-22 22:09:32 +0900

    feat: reset機能を追加

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
+const workTime = 25 * 60;
+
 const Timer = () => {
-  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [timeLeft, setTimeLeft] = useState(workTime);
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
@@ -26,11 +28,17 @@ const Timer = () => {
     return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`
   };
 
+  const onResetClick = () => {
+    setTimeLeft(workTime);
+    setIsActive(false);
+  };
+
   return (
     <div>
       <h2>{ formatTime(timeLeft) }</h2>
       <button onClick={ () => setIsActive(true) }>Start</button>
       <button onClick={ () => setIsActive(false) }>Stop</button>
+      <button onClick={ onResetClick }>Reset</button>
     </div>
   );
 };
