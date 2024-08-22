7de78be786b5fca45f2ed0b9861ef0ac43ae365b
su1415 <suyams@gmail.com>
2024-08-22 21:32:04 +0900

    feat: カウントダウン機能を追加

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Timer = () => {
   const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((timeLeft) => timeLeft - 1);
+    } , 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const formatTime = (second) => {
     const minutes = Math.floor(second / 60);
